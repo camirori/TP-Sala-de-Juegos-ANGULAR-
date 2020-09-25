54a7d49f3330d1c46ab9d042a32197789a92c02e
Camila Rori <camila.a.rori@gmail.com>
2020-09-25 03:20:38 -0300

    juegos sudoku

--- src/app/clases/juego-sudoku.ts
+++ src/app/clases/juego-sudoku.ts
@@ -1,76 +1,238 @@
 import { Juego } from '../clases/juego';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 export class JuegoSudoku extends Juego {
-    casilleros={0:undefined,1:undefined,2:undefined,3:undefined,
-        4:undefined,5:undefined,6:undefined,7:undefined,
-        8:undefined,9:undefined,10:undefined,11:undefined,
-        12:undefined,13:undefined,14:undefined,15:undefined,};
+    casilleros;
+    Tabla;
+    tablaInput;
+    numerosIngresados;
+    gano;
 
-    Tabla={ 
-        zona:{
-            1: {0: false, 1: false, 2: false, 3: false},
-            2: {0: false, 1: false, 2: false, 3: false},
-            3: {0: false, 1: false, 2: false, 3: false},
-            4: {0: false, 1: false, 2: false, 3: false},
-        },
-        fila:{
-            1: {0: false, 1: false, 2: false, 3: false},
-            2: {0: false, 1: false, 2: false, 3: false},
-            3: {0: false, 1: false, 2: false, 3: false},
-            4: {0: false, 1: false, 2: false, 3: false},
-        },
-        columna:{
-            1: {0: false, 1: false, 2: false, 3: false},
-            2: {0: false, 1: false, 2: false, 3: false},
-            3: {0: false, 1: false, 2: false, 3: false},
-            4: {0: false, 1: false, 2: false, 3: false},
-        }
+    asignarTabla(tamaño){
+        //if(tamaño==2){
+        let solvable;
 
-    };
+        do{            
+            this.Tabla={ 
+                        zona:{
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                        },
+                        fila:{
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                        },
+                        columna:{
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                        }
+                
+                    };
+            this.casilleros={0:undefined,1:undefined,2:undefined,3:undefined,
+                            4:undefined,5:undefined,6:undefined,7:undefined,
+                            8:undefined,9:undefined,10:undefined,11:undefined,
+                            12:undefined,13:undefined,14:undefined,15:undefined,};
+            
+
+            solvable=this.nuevoJuego2x2();                
+        }while(!solvable);
+
+
+        this.limpiarTablasInput();
+        /* }else{
+            this.Tabla={ 
+                        zona:{
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                            5: [],
+                            6: [],
+                            7: [],
+                            8: [],
+                            9: []
+                        },
+                        fila:{
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                            5: [],
+                            6: [],
+                            7: [],
+                            8: [],
+                            9: []
+                        },
+                        columna:{
+                            1: [],
+                            2: [],
+                            3: [],
+                            4: [],
+                            5: [],
+                            6: [],
+                            7: [],
+                            8: [],
+                            9: []
+                        }
+                
+                    };
+            this.casilleros={0:undefined,1:undefined,2:undefined,3:undefined,4:undefined,5:undefined,6:undefined,7:undefined,8:undefined,
+                            9:undefined,10:undefined,11:undefined,12:undefined,13:undefined,14:undefined,15:undefined,16:undefined,17:undefined,
+                            18:undefined,19:undefined,20:undefined,21:undefined,22:undefined,23:undefined,24:undefined,25:undefined,26:undefined,
+                            27:undefined,28:undefined,29:undefined,30:undefined,31:undefined,32:undefined,33:undefined,34:undefined,35:undefined,
+                            36:undefined,37:undefined,38:undefined,39:undefined,40:undefined,41:undefined,42:undefined,43:undefined,44:undefined,
+                            45:undefined,46:undefined,47:undefined,48:undefined,49:undefined,50:undefined,51:undefined,52:undefined,53:undefined,
+                            54:undefined,55:undefined,56:undefined,57:undefined,58:undefined,59:undefined,60:undefined,61:undefined,62:undefined,
+                            63:undefined,64:undefined,65:undefined,66:undefined,67:undefined,68:undefined,69:undefined,70:undefined,71:undefined,
+                            72:undefined,73:undefined,74:undefined,75:undefined,76:undefined,77:undefined,78:undefined,79:undefined,80:undefined,
+                        };
+            this.nuevoJuego3x3();
+        } */
+    }
+
+
 
     resultado;      
 
-    constructor(nombre?: string, gano?: boolean, jugador?:string) {
-        super("Memotest",gano,jugador);  
-        this.nuevoJuego();
+    constructor(tamaño?: number, nombre?: string, gano?: boolean, jugador?:string,) {
+        super("Memotest",gano,jugador);
+        this.asignarTabla(tamaño);
     }
 
-    nuevoJuego(){
+    nuevoJuego2x2(){
 
-        for(let i=0; i<16; i++){         //tarjetas
-            for(let j=0; j<2;j++){
-                console.log("Asignando tarjeta"+i +' '+j);
-                let casillero;
+        let casillero=0;
+        let z=1;
+        let c;
+        let f;
+        for(f=1;f<5;f++){         
+            if(z>f)
+                z-=2;                
+            for(c=1;c<5;c++){
+                let posibleNumero;
+                let stop=[];
                 do{
-                    casillero = Math.floor(Math.random()*16);
-                }while(this.casilleros[casillero]!=undefined);
-                this.casilleros[casillero]=i;                
+                    if(stop.length==4){
+                        console.log('No se puede resolver');
+                        return false;                            
+                    }
+                    posibleNumero=Math.floor(Math.random()*4)+1;
+                    if(!stop.includes(posibleNumero))
+                        stop.push(posibleNumero);
+                }while(this.Tabla.zona[z].includes(posibleNumero) || this.Tabla.fila[f].includes(posibleNumero)|| this.Tabla.columna[c].includes(posibleNumero)); 
+                this.Tabla.zona[z].push(posibleNumero);
+                this.Tabla.fila[f].push(posibleNumero);
+                this.Tabla.columna[c].push(posibleNumero);
+                this.casilleros[casillero]=posibleNumero;
+                casillero++;
+                console.log('f'+f+'z'+z+'c'+c);
+                if(c%2==0)
+                    z++;     
             }
+        }
+        console.log('Asignaciones:')
+        console.log(this.casilleros);        
+        return true;
+    }
 
+    /*nuevoJuego3x3(){
+        let casillero=0;
+        let z=1;
+        let c;
+        let f;
+        for(f=1;f<10;f++){         //tarjetas
+            //for(z=1;z<=f;){
+                if(z>f)
+                    z-=3;                
+                for(c=1;c<10;c++){
+                    let posibleNumero;
+                    let stop=[];
+                    do{
+                        if(stop.length==9){
+                            console.log('No se puede resolver');
+                            return;                            
+                        }
+                        posibleNumero=Math.floor(Math.random()*9)+1;
+                        if(!stop.includes(posibleNumero))
+                            stop.push(posibleNumero);
+                    }while(this.Tabla.zona[z].includes(posibleNumero) || this.Tabla.fila[f].includes(posibleNumero)|| this.Tabla.columna[c].includes(posibleNumero)); 
+                    this.Tabla.zona[z].push(posibleNumero);
+                    this.Tabla.fila[f].push(posibleNumero);
+                    this.Tabla.columna[c].push(posibleNumero);
+                    this.casilleros[casillero]=posibleNumero;
+                    casillero++;
+                    console.log('f'+f+'z'+z+'c'+c+' num'+posibleNumero);
+                    if(c%3==0)
+                        z++;     
+                }
+
+            //}
         }
         console.log('Asignaciones:')
         console.log(this.casilleros);
-    }
-
-    verificarJugada(casillero1, casillero2){
-        // if(this.casilleros[casillero1]==this.casilleros[casillero2]){
-        //  //   this.tarjetas[this.casilleros[casillero1]]=true;                //no volver a girar y disable
-        //     return true;
-        // }
-        // return false;                                                       //volver a girar y habilitar boton
-    }
+    }*/
 
     public verificar(){
-        //  //   for(let tarjeta in this.tarjetas){
-        //    //     if(this.tarjetas[tarjeta]==false)
-        //             return false;            //seguir jugando
-        //     }
-        //    // this.resultado=0;
-             return  true;          //gano
-
+        let casillero=0;
+        let z=1;
+        let c;
+        let f;
+        for(f=1;f<5;f++){       
+            if(z>f)
+                z-=2;                
+            for(c=1;c<5;c++){
+                if(this.tablaInput.zona[z].includes(this.numerosIngresados[casillero]) 
+                    || this.tablaInput.fila[f].includes(this.numerosIngresados[casillero])
+                    || this.tablaInput.columna[c].includes(this.numerosIngresados[casillero])){
+                        this.limpiarTablasInput();
+                        return false;                        
+                    }
+                this.tablaInput.zona[z].push(this.numerosIngresados[casillero]);
+                this.tablaInput.fila[f].push(this.numerosIngresados[casillero]);
+                this.tablaInput.columna[c].push(this.numerosIngresados[casillero]);
+                casillero++;
+                console.log('f'+f+'z'+z+'c'+c);
+                if(c%2==0)
+                    z++;     
+            }
+        }
+        this.gano=true;
+        return true;
     }
 
-    public calcularPuntaje(valorLineaGanadora){
+    limpiarTablasInput(){
+        this.tablaInput={ 
+            zona:{
+                1: [],
+                2: [],
+                3: [],
+                4: [],
+            },
+            fila:{
+                1: [],
+                2: [],
+                3: [],
+                4: [],
+            },
+            columna:{
+                1: [],
+                2: [],
+                3: [],
+                4: [],
+            }
+    
+        };
+        this.numerosIngresados=[];
+    }
+
+    public calcularPuntaje(tiempo){
 
 
     }
--- src/app/componentes/sudoku/sudoku.component.ts
+++ src/app/componentes/sudoku/sudoku.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JuegoSudoku } from 'src/app/clases/juego-sudoku';
 
 @Component({
   selector: 'app-sudoku',
@@ -6,10 +7,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sudoku.component.css']
 })
 export class SudokuComponent implements OnInit {
+  juego:  JuegoSudoku;
+  mensaje: string;
+  tiles;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.nuevoJuego();
+  }
+
+  grilla(){
+    this.tiles = [
+    {numero: 0, cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)' },
+    {numero: 1,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)'},
+    {numero: 2,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 3,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 4,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)'},
+    {numero: 5,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)'},
+    {numero: 6,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 7,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 8,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 9, cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 10,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)'},
+    {numero: 11,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)'},
+    {numero: 12,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 13,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(241, 241, 243)'},
+    {numero: 14,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)'},
+    {numero: 15,cols: 1, rows: 1, input: '', habilitar: true, color: 'rgb(177, 188, 238, 0.5)'}
+    ];
+  }
+
+
+
+  nuevoJuego(){
+    console.log('Nuevo juego');
+    this.grilla();
+    this.juego= new JuegoSudoku(2);
+    this.generarAyuda();
+    this.mensaje=undefined;
+  }
+
+  generarAyuda(){
+    let pista;
+    for(let i=0;i<5;i++){
+      do{
+        pista= Math.floor(Math.random()*16);        
+      }while(this.tiles[pista].input!='');
+        this.tiles[pista].input=this.juego.casilleros[pista];
+        this.tiles[pista].habilitar=false;
+        this.tiles[pista].color='rgb(126, 140, 204, 0.6)';
+    }
+  }
+
+  verificar(){
+    for(let i=0;i<16;i++){
+        this.juego.numerosIngresados[i]= this.tiles[i].input;
+    }
+    console.log('Verificando input:')
+    console.log(this.juego.numerosIngresados);
+
+    if(this.juego.numerosIngresados.includes('') || this.juego.numerosIngresados.includes(null))
+      this.mensaje='Aún hay casilleros vacíos';
+    else if(this.juego.verificar())
+      this.mensaje='Ganaste!';
+    else
+      this.mensaje='Incorrecto';
+
+  }
+
+  mostrarRespuesta(){
+    for(let i=0;i<16;i++){
+      this.tiles[i].input= this.juego.casilleros[i];
+  }
   }
 
 }
